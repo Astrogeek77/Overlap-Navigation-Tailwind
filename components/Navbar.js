2fa45d33552b954c8d9a788326b51eb0998e64d3
Gautam Jain <astrogeek77@gmail.com>
2022-01-24 21:40:35 +0530

    Updated Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -74,7 +74,6 @@ function Navbar() {
                       <BellIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
 
-                    {/* Profile dropdown */}
                     <Menu as="div" className="ml-3 relative">
                       <div>
                         <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
